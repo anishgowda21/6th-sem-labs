62295a45809447eec2a8b12d4a750a6044b23f45
Anish Gowda <anishgowda43@gmail.com>
2022-08-06 18:41:52 +0530

    changed from mongoose to mongodb

--- weblab/ExamProblems/nodeJs/cie_marks/server.js
+++ weblab/ExamProblems/nodeJs/cie_marks/server.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../db');
-const mongoose = require('mongoose');
-db.connect('cie_marks');
-
+const { json } = require('body-parser');
+const mongo = require('mongodb').MongoClient;
 const app = express();
-
+const url = 'mongodb://127.0.0.1:27017/cie_marks';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
-
-var stdSchema = new mongoose.Schema({
-    usn: String,
-    name: String,
-    subCode: String,
-    marks: Number
-});
-var student = mongoose.model('student', stdSchema);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -26,35 +16,47 @@ app.get('/', (req, res) => {
 app.get("/getData", (req, res) => {
     var data = req.query;
     console.log(data);
-    var mod = new student(data);
-    mod.save((err, result) => {
+    // Must use mongodb only in exam also
+    mongo.connect(url, (err, db) => {
         if (err) {
-            res.send(err);
+            console.log(err);
+            process.exit(1);
         }
-        else {
-            res.header("content-type", "text/html");
-            res.send("Data Inserted<br>" + result);
-        }
+        var collection = db.collection('students');
+        collection.insert(data, (err, result) => {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            res.send(result);
+        });
     })
 })
 
 app.get("/showData", (req, res) => {
     //Get student with less then 20 marks
-    var query = { marks: { $lt: 20 } };
-    student.find(query, (err, result) => {
+    mongo.connect(url, (err, db) => {
         if (err) {
-            res.send(err);
+            console.log(err);
+            process.exit(1);
         }
-        else {
+        var collection = db.collection('students');
+        var query = { marks: { $lt: 20 } };
+        collection.find(query).toArray((err, result) => {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
             var text = "<h2>Students with less then 20 marks</h2> <br>";
             text += "<ul>"
             res.header("content-type", "text/html");
             result.forEach(element => {
-                text += "<li>" + element + "</li>";
+                text += "<li>" + JSON.stringify(element) + "</li>";
             });
             text += "</ul>";
             res.send(text);
-        }
+            db.close();
+        })
     })
 })
 
--- weblab/ExamProblems/nodeJs/examFee/server.js
+++ weblab/ExamProblems/nodeJs/examFee/server.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../db');
-const mongoose = require('mongoose');
-db.connect('exam_fee');
+const mongo = require('mongodb').MongoClient;
 
 const app = express();
-
+const url = 'mongodb://127.0.0.1:27017/exam_fee';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-var stdSchema = new mongoose.Schema({
-    usn: String,
-    name: String,
-    semester: Number,
-    examFee: {
-        type: Number,
-        default: 0
-    }
-});
-var student = mongoose.model('student', stdSchema);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -28,36 +16,48 @@ app.get('/', (req, res) => {
 
 app.get("/getData", (req, res) => {
     var data = req.query;
-    data.examFee = data.examFee ? data.examFee : 0;
-    var mod = new student(data);
-    mod.save((err, result) => {
+    data.examFee = data.examFee ? parseInt(data.examFee) : 0;
+    mongo.connect(url, (err, db) => {
         if (err) {
-            res.send(err);
+            console.log(err);
+            process.exit(1);
         }
-        else {
-            res.header("content-type", "text/html");
-            res.send("Data Inserted<br>" + result);
-        }
+        var collection = db.collection('students');
+        collection.insert(data, (err, result) => {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.header("content-type", "text/html");
+                res.send("Data Inserted<br>" + JSON.stringify(result));
+            }
+        })
     })
 })
 
 app.get("/showData", (req, res) => {
     // Get Student with exam fee equal to 0
     var query = { examFee: 0 };
-    student.find(query, (err, result) => {
+    mongo.connect(url, (err, db) => {
         if (err) {
-            res.send(err);
+            console.log(err);
+            process.exit(1);
         }
-        else {
+        var collection = db.collection('students');
+        collection.find(query).toArray((err, result) => {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
             var text = "<h2>students who have not paid exam fees</h2> <br>";
             text += "<ul>"
             res.header("content-type", "text/html");
             result.forEach(element => {
-                text += "<li>" + element + "</li>";
+                text += "<li>" + JSON.stringify(element) + "</li>";
             });
             text += "</ul>";
             res.send(text);
-        }
+        })
     })
 })
 
